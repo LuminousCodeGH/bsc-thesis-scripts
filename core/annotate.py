627619f58660cb3afddabfad8c95c10606dbb04c
Jacob <Jacob McCain>
2024-05-13 01:59:48 +0200

    Add recommended annotation for cogdx

--- core/annotate.py
+++ core/annotate.py
@@ -1,6 +1,9 @@
 import anndata as ad
 
 
+COGDX_MAP = {1: 'H', 2: 'M', 3: 'M', 4: 'AD', 5: 'AD', 6: 'O'}
+
+
 def _categorize_by(adata: ad.AnnData, by: str, map: dict[int, str], col_name: str, inplace=True) -> None | ad.AnnData:
     assert all(u_entry in map.keys() for u_entry in adata.obs[by].unique()), \
         f'The map is missing entries! \nmap: {map.keys()}\nentries: {adata.obs[by].unique().tolist()}'
