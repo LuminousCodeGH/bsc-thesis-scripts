cfc6c9622c82c7236f3f152f74cf160d95c1d7bb
Jacob <Jacob McCain>
2024-05-12 19:26:26 +0200

    Streamline X and Y inputs and possibly fix a bug

--- core/test_model.py
+++ core/test_model.py
@@ -10,19 +10,16 @@ from statsmodels.stats.multitest import fdrcorrection
 
 
 def test_model(adata: ad.AnnData, 
-                model: ClassifierMixin=RandomForestClassifier(class_weight='balanced'), 
-                y: pd.Series=None, 
-                norm_layer: str='norm', 
-                significance_col: str='significant',
-                sig_filter: bool=False) -> pd.DataFrame:
+               model: ClassifierMixin, 
+               y: pd.Series, 
+               norm_layer: str='norm', 
+               significance_col: str='significant',
+               sig_filter: bool=False) -> pd.DataFrame:
     if sig_filter:
         X = pd.DataFrame(adata.layers[norm_layer][:, adata.var[significance_col] == True])
     else:
         X = pd.DataFrame(adata.layers[norm_layer])
-
-    if y is None:
-        y = (adata.obs['cogdx'] == 5) | (adata.obs['cogdx'] == 4)
-
+    
     cv_res = kfold_cv(model, X, y)
     adata.uns['model_training_results'] = cv_res
 
@@ -34,8 +31,13 @@ def test_abundance(adata: ad.AnnData,
                    corr_func: Callable=fdrcorrection,
                    norm_layer: str='norm',
                    alpha: float=0.05,
+                   X_filter: pd.Series=None,
                    Y_filter: pd.Series=None) -> None:
-    X = adata.layers[norm_layer][adata.obs['cogdx'] == 1]
+    if X_filter is None:
+        X = adata.layers[norm_layer][adata.obs['cogdx'] == 1]
+    else:
+        X = adata.layers[norm_layer][X_filter]
+
     if Y_filter is None:
         Y = adata.layers[norm_layer][(adata.obs['cogdx'] == 4) | (adata.obs['cogdx'] == 5)]
     else:
@@ -54,10 +56,10 @@ def test_model_from_scratch(adata: ad.AnnData,
                              layer_name: str='norm',
                              model: ClassifierMixin=RandomForestClassifier(class_weight='balanced'), 
                              alpha: float=0.05,
-                             Y_filter: pd.Series=None,
+                             y: pd.Series=None,
                              sig_filter: bool=False) -> pd.DataFrame:
     '''Default test is between cogdx=1 and cogdx=4/5 on a RandomForestClassifier'''
     norm_func(adata, layer_name=layer_name)
-    test_abundance(adata, ranksums, fdrcorrection, norm_layer=layer_name, alpha=alpha, Y_filter=Y_filter)
+    test_abundance(adata, ranksums, fdrcorrection, norm_layer=layer_name, alpha=alpha)
 
-    return test_model(adata, model, Y_filter, norm_layer=layer_name, sig_filter=sig_filter)
+    return test_model(adata, model, y, norm_layer=layer_name, sig_filter=sig_filter)
